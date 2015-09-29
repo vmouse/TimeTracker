a80019d70b7995251c6a5816395d331618b3d5a1
vmouse <vlad.baginsky@gmail.com>
2015-09-29 23:24:09 +0300

    First commit

---
+++ TimeTracker/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace TimeTracker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbLog.Location = new System.Drawing.Point(12, 41);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbLog.Size = new System.Drawing.Size(470, 208);
+            this.tbLog.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 261);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
+            this.Text = "Start";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbLog;
+    }
+}
+
---
+++ TimeTracker/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SharePoint.Client;
+using System.Security;
+
+
+namespace TimeTracker
+{
+    public partial class MainForm : System.Windows.Forms.Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (ClientContext cont = new ClientContext("https://inblago.sharepoint.com"))
+            {
+                SecureString pass = new SecureString();
+                foreach (char c in "password".ToCharArray()) pass.AppendChar(c);
+                cont.Credentials = new SharePointOnlineCredentials("login", pass);
+                Web oWeb = cont.Web;
+                cont.Load(oWeb);
+                List tasks = oWeb.Lists.GetByTitle("Tasks");
+                oWeb.Lists.RetrieveItems().Retrieve();
+
+                CamlQuery caml = new Microsoft.SharePoint.Client.CamlQuery();
+                caml.ViewXml = "<View Scope='RecursiveAll' />";
+                ListItemCollection items = tasks.GetItems(caml);
+                items.RetrieveItems().Retrieve();
+                cont.ExecuteQuery();
+                // foreach (List lst in oWeb.Lists)
+                foreach (ListItem item in items)
+                {
+                    tbLog.AppendText(item.FieldValues["Title"]+"\n");
+                }
+            }
+        }
+    }
+}
